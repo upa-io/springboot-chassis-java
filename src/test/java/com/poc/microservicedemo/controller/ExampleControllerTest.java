a208308c7b1bd68e56247b197f67c401d05d527a
Jesus Aguirre <info.aguirre.jesus@gmail.com>
2022-08-23 21:01:15 -0500

    Agregar assert en prueba de POST

--- src/test/java/com/poc/microservicedemo/controller/ExampleControllerTest.java
+++ src/test/java/com/poc/microservicedemo/controller/ExampleControllerTest.java
@@ -8,8 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,9 +39,9 @@ class ExampleControllerTest {
     ServiceRequestDTO request = new ServiceRequestDTO ();
     request.setGreeting("Hello!");
 
-    mockMvc.perform(MockMvcRequestBuilders.post("/v1/greeting").content(asJsonString(request))
+    assertNotNull(mockMvc.perform(MockMvcRequestBuilders.post("/v1/greeting").content(asJsonString(request))
             .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON));
+            .accept(MediaType.APPLICATION_JSON)));
   }
 
   public static String asJsonString(final Object obj) {
